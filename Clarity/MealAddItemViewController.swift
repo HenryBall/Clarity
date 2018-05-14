14fe79ec8de12fd9400c5dccfe5df6afc7ddcf7d
Celine Pena <celinenpena@gmail.com>
2018-05-14 15:51:20 -0700

    Semi laid out UI on meal screen/segues

---
+++ Clarity/MealAddItemViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MealAddItemViewController.swift
+//  Clarity
+//
+//  Created by Celine Pena on 5/14/18.
+//  Copyright © 2018 Robert. All rights reserved.
+//
+
+import UIKit
+
+class MealAddItemViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func backTapped(_ sender: UIButton) {
+        self.navigationController?.popViewController(animated: true)
+    }
+}
--- Clarity/MealViewController.swift
+++ Clarity/MealViewController.swift
@@ -8,13 +8,19 @@
 
 import UIKit
 
-class MealViewController: UIViewController {
+class MealViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     var selectedOption: Int!
     @IBOutlet weak var bannerImage: UIImageView!
+    @IBOutlet weak var gallonsInMeal: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var tableViewHeight: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
         setUpView()
         // Do any additional setup after loading the view.
     }
@@ -35,5 +41,23 @@ class MealViewController: UIViewController {
     }
     @IBAction func backTapped(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
+    }
+    @IBAction func addTapped(_ sender: UIButton) {
+        let destination = self.storyboard?.instantiateViewController(withIdentifier: "MealAddItemViewController") as! MealAddItemViewController
+        self.navigationController?.pushViewController(destination, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 60.0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "IngredientCell") as! IngredientCell
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        tableViewHeight.constant = 10*60.0
+        return 10
     }
 }
