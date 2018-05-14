f53be2171ca569ab6bde2136dbff86abfcdeeed2
Celine Pena <celinenpena@gmail.com>
2018-05-14 12:18:48 -0700

    Google login works
    Basic login functionality

--- Clarity/LoginViewController.swift
+++ Clarity/LoginViewController.swift
@@ -7,18 +7,24 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, GIDSignInUIDelegate {
+    
+    @IBOutlet weak var GoogleSignInButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(sender:)))
+        GoogleSignInButton.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    @objc func handleTap(sender: UITapGestureRecognizer? = nil) {
+        GIDSignIn.sharedInstance().uiDelegate = self
+        GIDSignIn.sharedInstance().signIn()
     }
 
 }
---
+++ Podfile
@@ -0,0 +1,30 @@
+platform :ios, '9.0'
+
+target 'Clarity' do
+# Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+use_frameworks!
+
+# Pods for Clarity
+pod 'SwiftyJSON', '~> 3.1'
+pod 'Alamofire'
+pod 'Firebase/Core'
+pod 'Firebase/Firestore'
+pod 'Firebase/Storage'
+pod 'SDWebImage', '~> 4.0'
+pod 'FirebaseUI/Storage', '~> 4.0'
+pod 'Charts'
+pod 'ChartsRealm'
+pod 'GoogleSignIn'
+pod 'Firebase/Auth'
+
+target 'ClarityTests' do
+inherit! :search_paths
+# Pods for testing
+end
+
+target 'ClarityUITests' do
+inherit! :search_paths
+# Pods for testing
+end
+
+end
