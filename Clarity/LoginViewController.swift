070a0bf1a6ecc0d476fe7d029a5d1f7c1e032381
Celine Pena <celinenpena@gmail.com>
2018-05-14 10:51:53 -0700

    added navigation controller

---
+++ Clarity/LoginViewController.swift
@@ -0,0 +1,24 @@
+//
+//  LoginViewController.swift
+//  Clarity
+//
+//  Created by Celine Pena on 5/14/18.
+//  Copyright © 2018 Robert. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.navigationController?.isNavigationBarHidden = true
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
