e21cb397c9bd3ec99334452a14f053cde635aef5
HenryBall <hfball@ucsc.edu>
2018-05-31 23:09:42 -0700

    refactored firebase queries

--- Clarity/Helpers/firebaseHelper.swift
+++ Clarity/Helpers/firebaseHelper.swift
@@ -11,34 +11,162 @@ import Firebase
 import Foundation
 
 class firebaseHelper {
+    
     let db = Firestore.firestore()
     
-    static func getTotalForDay(day: DocumentReference, completionHandler: @escaping (Double, Error?) -> Void) {
+    static func getBreakfastTotalForDay(day: DocumentReference, completion: @escaping (Double, Error?) -> Void) {
         var total = 0.0
         let group = DispatchGroup()
+        
         group.enter()
         day.getDocument { (document, error) in
             if (document?.exists)! {
-                let breakfastRef = document?.data()!["breakfast"] as! [DocumentReference]
-                for ref in breakfastRef {
-                    group.enter()
-                    ref.getDocument { (doc, error) in
-                        if let doc = doc, doc.exists {
-                            total = total + (doc.data()!["gallons_water"] as! Double)
-                        } else {
-                            print("Document does not exist")
+                if let breakfastRef = document?.data()!["breakfast"] {
+                    let refArray = breakfastRef as! [DocumentReference]
+                    for ref in refArray {
+                        group.enter()
+                        ref.getDocument { (doc, error) in
+                            if let doc = doc, doc.exists {
+                                total = total + (doc.data()!["gallons_water"] as! Double)
+                            } else {
+                                completion(0.0, ("Breakfast error" as! Error))
+                            }
+                            group.leave()
                         }
-                        group.leave()
                     }
                 }
             } else {
-                print("Error")
+                completion(0.0, ("Breakfast error" as! Error))
             }
             group.leave()
         }
         
         group.notify(queue: .main) {
-            completionHandler(total, nil)
+            completion(total, nil)
+        }
+    }
+    
+    static func getLunchTotalForDay(day: DocumentReference, completion: @escaping (Double, Error?) -> Void) {
+        var total = 0.0
+        let group = DispatchGroup()
+        
+        group.enter()
+        day.getDocument { (document, error) in
+            if (document?.exists)! {
+                if let lunchRef = document?.data()!["lunch"] {
+                    let refArray = lunchRef as! [DocumentReference]
+                    for ref in refArray {
+                        group.enter()
+                        ref.getDocument { (doc, error) in
+                            if let doc = doc, doc.exists {
+                                total = total + (doc.data()!["gallons_water"] as! Double)
+                            } else {
+                                completion(0.0, ("Lunch error" as! Error))
+                            }
+                            group.leave()
+                        }
+                    }
+                }
+            } else {
+                completion(0.0, ("Lunch error" as! Error))
+            }
+            group.leave()
+        }
+        
+        group.notify(queue: .main) {
+            completion(total, nil)
+        }
+    }
+    
+    static func getDinnerTotalForDay(day: DocumentReference, completion: @escaping (Double, Error?) -> Void) {
+        var total = 0.0
+        let group = DispatchGroup()
+        
+        group.enter()
+        day.getDocument { (document, error) in
+            if (document?.exists)! {
+                if let dinnerRef = document?.data()!["dinner"] {
+                    let refArray = dinnerRef as! [DocumentReference]
+                    for ref in refArray {
+                        group.enter()
+                        ref.getDocument { (doc, error) in
+                            if let doc = doc, doc.exists {
+                                total = total + (doc.data()!["gallons_water"] as! Double)
+                            } else {
+                                completion(0.0, ("Dinner error" as! Error))
+                            }
+                            group.leave()
+                        }
+                    }
+                }
+            } else {
+                completion(0.0, ("Dinner error" as! Error))
+            }
+            group.leave()
+        }
+        
+        group.notify(queue: .main) {
+            completion(total, nil)
+        }
+    }
+    
+    static func getSnackTotalForDay(day: DocumentReference, completion: @escaping (Double, Error?) -> Void) {
+        var total = 0.0
+        let group = DispatchGroup()
+        
+        group.enter()
+        day.getDocument { (document, error) in
+            if (document?.exists)! {
+                if let dinnerRef = document?.data()!["snacks"] {
+                    let refArray = dinnerRef as! [DocumentReference]
+                    for ref in refArray {
+                        group.enter()
+                        ref.getDocument { (doc, error) in
+                            if let doc = doc, doc.exists {
+                                total = total + (doc.data()!["gallons_water"] as! Double)
+                            } else {
+                                completion(0.0, ("Snack error" as! Error))
+                            }
+                            group.leave()
+                        }
+                    }
+                }
+            } else {
+                completion(0.0, ("Snack error" as! Error))
+            }
+            group.leave()
+        }
+        
+        group.notify(queue: .main) {
+            completion(total, nil)
+        }
+    }
+    
+    static func getTotalForDay(day: DocumentReference, completion: @escaping (Double, Error?) -> Void) {
+        getBreakfastTotalForDay(day: day) { (bTotal, error) in
+            if let error = error {
+                print (error)
+            } else {
+                getLunchTotalForDay(day: day) { (lTotal, error) in
+                    if let error = error {
+                        print (error)
+                    } else {
+                        getDinnerTotalForDay(day: day) { (dTotal, error) in
+                            if let error = error {
+                                print (error)
+                            } else {
+                                getSnackTotalForDay(day: day) { (sTotal, error) in
+                                    if let error = error {
+                                        print (error)
+                                    } else {
+                                        completion((bTotal + lTotal + dTotal + sTotal), nil)
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
--- Clarity/HomeViewController.swift
+++ Clarity/HomeViewController.swift
@@ -81,6 +81,36 @@ class HomeViewController: UIViewController, UIScrollViewDelegate {
         }
     }
     
+    /* IBActions ********************************************************************************************************/
+    @IBAction func breakfastTapped(_ sender: Any) {
+        let destination = self.storyboard?.instantiateViewController(withIdentifier: "MealViewController") as! MealViewController
+        destination.mealType = "breakfast"
+        self.navigationController?.pushViewController(destination, animated: true)
+    }
+    
+    @IBAction func lunchTapped(_ sender: Any) {
+        let destination = self.storyboard?.instantiateViewController(withIdentifier: "MealViewController") as! MealViewController
+        destination.mealType = "lunch"
+        self.navigationController?.pushViewController(destination, animated: true)
+    }
+    
+    @IBAction func dinnerTapped(_ sender: Any) {
+        let destination = self.storyboard?.instantiateViewController(withIdentifier: "MealViewController") as! MealViewController
+        destination.mealType = "dinner"
+        self.navigationController?.pushViewController(destination, animated: true)
+    }
+    
+    @IBAction func snacksTapped(_ sender: Any) {
+        let destination = self.storyboard?.instantiateViewController(withIdentifier: "MealViewController") as! MealViewController
+        destination.mealType = "snacks"
+        self.navigationController?.pushViewController(destination, animated: true)
+    }
+    
+    @IBAction func settingsBtnTapped(_ sender: Any) {
+        let destination = self.storyboard?.instantiateViewController(withIdentifier: "SettingsViewController") as! SettingsViewController
+        self.navigationController?.pushViewController(destination, animated: true)
+    }
+    
     /* Queries ************************************************************************************************/
     
     ///Queries all of the ingredients from our database: water-footprint-data, adds them to array ingredientsFromDatabase (a global array used throughout the application)
@@ -119,87 +149,19 @@ class HomeViewController: UIViewController, UIScrollViewDelegate {
         
         firebaseHelper.getTotalForDay(day: day) { (total, error) in
             if let error = error {
-                print ("Error")
-            }
-            
-            self.drawCircle(greenRating: CGFloat(total))
-            self.dailyTotalLabel.text = String(Int(total))
-        }
-    }
-    
-    /*func queryTotal(){
-        //var breakfastTotal = 0.0
-        //var lunchTotal = 0.0
-        //var dinnerTotal = 0.0
-        //var snacksTotal = 0.0
-        //var total = 0.0
-        
-        let day = db.collection("users").document(defaults.string(forKey: "user_id")!).collection("meals").document(databaseDateFormatter.string(from: today))
-        
-        day.getDocument { (document, error) in
-            if(document?.exists)!{
-                /*if let breakfast_total = document?.data()!["breakfast_total"]{
-                    breakfastTotal = breakfast_total as! Double
+                print (error)
+            } else {
+                let totalAsString = String(Int(total))
+                if (totalAsString != self.dailyTotalLabel.text) {
+                    self.drawCircle(dailyTotal: CGFloat(total))
+                    self.dailyTotalLabel.text = totalAsString
                 }
-                
-                if let lunch_total = document?.data()!["lunch_total"]{
-                    lunchTotal = lunch_total as! Double
-                }
-                
-                if let dinner_total = document?.data()!["dinner_total"]{
-                    dinnerTotal = dinner_total as! Double
-                }
-                
-                if let snacks_total = document?.data()!["snacks_total"]{
-                    snacksTotal = snacks_total as! Double
-                }*/
-                
-                //total = breakfastTotal + lunchTotal + dinnerTotal + snacksTotal
-                //self.dailyTotalLabel.text = String(Int(total))
-                if(total != self.totalGallonsToday) {
-                    self.drawCircle(greenRating: CGFloat(total))
-                    //self.updatePieChart(breakfast: breakfastTotal, lunch: lunchTotal, dinner: dinnerTotal, snacks: snacksTotal)
-                    self.totalGallonsToday = total
-                }
-            }else{
-                print("called draw circle with 0")
-                self.drawCircle(greenRating: 0.0)
             }
         }
-    }*/
-    
-    /* IBAction ********************************************************************************************************/
-    @IBAction func breakfastTapped(_ sender: Any) {
-        let destination = self.storyboard?.instantiateViewController(withIdentifier: "MealViewController") as! MealViewController
-        destination.mealType = "breakfast"
-        self.navigationController?.pushViewController(destination, animated: true)
-    }
-    
-    @IBAction func lunchTapped(_ sender: Any) {
-        let destination = self.storyboard?.instantiateViewController(withIdentifier: "MealViewController") as! MealViewController
-        destination.mealType = "lunch"
-        self.navigationController?.pushViewController(destination, animated: true)
-    }
-    
-    @IBAction func dinnerTapped(_ sender: Any) {
-        let destination = self.storyboard?.instantiateViewController(withIdentifier: "MealViewController") as! MealViewController
-        destination.mealType = "dinner"
-        self.navigationController?.pushViewController(destination, animated: true)
-    }
-    
-    @IBAction func snacksTapped(_ sender: Any) {
-        let destination = self.storyboard?.instantiateViewController(withIdentifier: "MealViewController") as! MealViewController
-        destination.mealType = "snacks"
-        self.navigationController?.pushViewController(destination, animated: true)
-    }
-    
-    @IBAction func settingsBtnTapped(_ sender: Any) {
-          let destination = self.storyboard?.instantiateViewController(withIdentifier: "SettingsViewController") as! SettingsViewController
-          self.navigationController?.pushViewController(destination, animated: true)
     }
     
     /* Circle Animation ************************************************************************************************/
-    func drawCircle(greenRating: CGFloat) {
+    func drawCircle(dailyTotal: CGFloat) {
         let trackLayer = CAShapeLayer()
         let center = circleView.center
         let circularPath = UIBezierPath(arcCenter: center, radius: 90, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
@@ -213,10 +175,10 @@ class HomeViewController: UIViewController, UIScrollViewDelegate {
         view.layer.addSublayer(trackLayer)
         scrollView.layer.addSublayer(trackLayer)
         
-        let percentage = Int(( greenRating / CGFloat(dailyGoal)) * 100)
+        let percentage = Int(( dailyTotal / CGFloat(dailyGoal)) * 100)
         percentLabel.text = String(describing: percentage) + "% of your daily limit"
 
-        let endAngle = (CGFloat.pi / 2) + (CGFloat.pi * 2 * (greenRating / CGFloat(dailyGoal)))
+        let endAngle = (CGFloat.pi / 2) + (CGFloat.pi * 2 * (dailyTotal / CGFloat(dailyGoal)))
         let motionPath = UIBezierPath(arcCenter: center, radius: 90, startAngle: CGFloat.pi / 2, endAngle: endAngle , clockwise: true)
         shapeLayer.path = motionPath.cgPath
         
@@ -250,133 +212,5 @@ class HomeViewController: UIViewController, UIScrollViewDelegate {
         }
         self.pageControl.currentPage = Int(currentPage)
     }
-    
-    
-    
-    /* Charts ************************************************************************************************/
-    //Bar Chart
-    /*func updateChartWithData(allWaterData: [Double]) {
-     var dataEntries: [BarChartDataEntry] = []
-     
-     for i in 0..<allWaterData.count {
-     let dataEntry = BarChartDataEntry(x: Double(i), y: Double(allWaterData[i]))
-     dataEntries.append(dataEntry)
-     }
-     
-     
-     let chartDataSet = BarChartDataSet(values: dataEntries, label: "Gallons of water per day")
-     barChart.xAxis.labelPosition = .bottom
-     chartDataSet.drawValuesEnabled = false
-     
-     let colors = Array(repeating: UIColor.white, count: allWaterData.count)
-     chartDataSet.colors = colors
-     
-     let limitLine = ChartLimitLine(limit: dailyGoal, label: "") //show daily limit on graph
-     limitLine.lineColor = UIColor.white
-     limitLine.lineWidth = 1
-     
-     barChart.xAxis.drawGridLinesEnabled = false //hide vertical grid lines
-     barChart.xAxis.drawLabelsEnabled = false //hide x axis labels
-     barChart.drawValueAboveBarEnabled = false
-     //barChart.rightAxis.addLimitLine(limitLine) //show daily limit line on graph
-     
-     barChart.rightAxis.addLimitLine(limitLine) //show daily limit line on graph
-     barChart.rightAxis.drawGridLinesEnabled = false //hide vertical line on right side
-     barChart.rightAxis.drawAxisLineEnabled = false //hide horizontal axis lines
-     barChart.rightAxis.labelTextColor = UIColor.white
-     
-     barChart.leftAxis.drawLabelsEnabled = false //hide axis labels on left side
-     barChart.leftAxis.drawGridLinesEnabled = false //hide horizontal axis lines
-     barChart.leftAxis.drawAxisLineEnabled = false //hide vertical line on right side
-     
-     barChart.drawBordersEnabled = false //hide borders
-     
-     barChart.noDataTextColor = UIColor.white //set color of "no data available" text
-     barChart.chartDescription?.text = "" //hide description under the chart
-     barChart.legend.enabled = false //hide legend
-     barChart.isUserInteractionEnabled = false
-     barChart.data = BarChartData(dataSet: chartDataSet)
-     }*/
-    
-    //Pie Chart
-    /*func updatePieChart(breakfast: Double, lunch: Double, dinner: Double, snacks: Double)  {
-     
-     let meals = [breakfast, lunch, dinner, snacks]
-     let track = ["Breakfast", "Lunch", "Dinner", "Snacks"]
-     var entries = [PieChartDataEntry]()
-     for (index, value) in meals.enumerated() {
-     let entry = PieChartDataEntry()
-     entry.y = value
-     entry.label = track[index]
-     entries.append( entry)
-     }
-     
-     let set = PieChartDataSet(values: entries, label: "")
-     set.colors = ChartColorTemplates.joyful()
-     set.drawValuesEnabled = false
-     
-     let data = PieChartData(dataSet: set)
-     pieChart.data = data
-     pieChart.chartDescription?.text = ""
-     pieChart.noDataText = "No data available"
-     pieChart.noDataTextColor = UIColor.white
-     pieChart.legend.horizontalAlignment = .right
-     pieChart.legend.verticalAlignment = .center
-     pieChart.legend.orientation = .vertical
-     pieChart.legend.textColor = UIColor.white
-     pieChart.drawEntryLabelsEnabled = false
-     pieChart.isUserInteractionEnabled = false
-     
-     pieChart.holeRadiusPercent = 0.5
-     pieChart.holeColor = UIColor.clear
-     pieChart.transparentCircleColor = UIColor.clear
-     }*/
-    
-    ///Queries the user's last 10 days total water intake
-    /*func getBarGraphData(){
-     var waterData = [Double]()
-     var breakfastTotal = 0.0
-     var lunchTotal = 0.0
-     var dinnerTotal = 0.0
-     var snacksTotal = 0.0
-     
-     let day = db.collection("users").document(user).collection("meals")
-     day.getDocuments { (querySnapshot, err) in
-     if err != nil {
-     print("Error getting bar graph data")
-     }else{
-     for document in querySnapshot!.documents.reversed(){
-     if(waterData.count == 10){
-     return
-     }else{
-     if let breakfast_total = document.data()["breakfast_total"]{
-     breakfastTotal = breakfast_total as! Double
-     }
-     
-     if let lunch_total = document.data()["lunch_total"]{
-     lunchTotal = lunch_total as! Double
-     }
-     
-     if let dinner_total = document.data()["dinner_total"]{
-     dinnerTotal = dinner_total as! Double
-     }
-     
-     if let snacks_total = document.data()["snacks_total"]{
-     snacksTotal = snacks_total as! Double
-     }
-     
-     let total = breakfastTotal + lunchTotal + dinnerTotal + snacksTotal
-     waterData.append(total)
-     }
-     }
-     if (waterData.count < 10) {
-     for _ in 1...(10 - waterData.count) {
-     waterData.append(0.0)
-     }
-     }
-     //self.updateChartWithData(allWaterData: waterData.reversed())
-     }
-     }
-     }*/
-}
 
+}
