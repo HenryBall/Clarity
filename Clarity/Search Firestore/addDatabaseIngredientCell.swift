c024ff873a4ce36a25050d04864dd323fc201789
Celine Pena <celinenpena@gmail.com>
2018-08-18 21:33:14 -0700

    forgot one file
    oops

--- Clarity/Search Firestore/addDatabaseIngredientCell.swift
+++ Clarity/Search Firestore/addDatabaseIngredientCell.swift
@@ -14,15 +14,16 @@ class addDatabaseIngredientCell: UITableViewCell {
     @IBOutlet weak var label: UILabel!
     @IBOutlet weak var count: UITextField!
     @IBOutlet weak var gallonsWaterLabel: UILabel!
+    @IBOutlet weak var addButton: UIButton!
     
     @IBOutlet weak var quantityTextField: UITextField!
     override func awakeFromNib() {
         super.awakeFromNib()
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-        accessoryType = selected ? .checkmark : .none
-    }
+//    override func setSelected(_ selected: Bool, animated: Bool) {
+//        super.setSelected(selected, animated: animated)
+//        accessoryType = selected ? .checkmark : .none
+//    }
 
 }
