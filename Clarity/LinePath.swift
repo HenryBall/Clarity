08f61d444792e8f426d2ccaf2ed3611b29fb5147
henry <henry@henryss-iMac.local>
2018-09-15 16:17:51 -0700

    last commit before big changes

---
+++ Clarity/LinePath.swift
@@ -0,0 +1,21 @@
+//
+//  LinePath.swift
+//  Clarity
+//
+//  Created by henry on 9/12/18.
+//  Copyright © 2018 Robert. All rights reserved.
+//
+
+import UIKit
+
+class LinePath: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
